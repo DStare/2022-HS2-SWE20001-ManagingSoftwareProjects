b4a07ae1f02d624a29937064c877d66b778125d8
Dancho <danielmarkovski2009@hotmail.com>
2022-10-11 14:14:55 +1100

    Fixed error where program would not launch due to process being using by visual studio for something else. Added some code for login page with text boxes are blank

--- go2grocery_1/LoginForm.Designer.cs
+++ go2grocery_1/LoginForm.Designer.cs
@@ -49,13 +49,11 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Rockwell", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.RosyBrown;
-            this.label1.Image = global::go2grocery_1.Properties.Resources.go2grocery;
             this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.label1.ImageKey = "(none)";
-            this.label1.Location = new System.Drawing.Point(-1, -3);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(-1, -2);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(609, 58);
+            this.label1.Size = new System.Drawing.Size(409, 39);
             this.label1.TabIndex = 2;
             this.label1.Text = "Go2Grocery Login Page";
             this.label1.UseMnemonic = false;
@@ -63,35 +61,30 @@
             // 
             // UsernameBox
             // 
-            this.UsernameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UsernameBox.ForeColor = System.Drawing.SystemColors.InactiveCaption;
-            this.UsernameBox.Location = new System.Drawing.Point(476, 223);
-            this.UsernameBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.UsernameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsernameBox.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.UsernameBox.Location = new System.Drawing.Point(317, 145);
             this.UsernameBox.Name = "UsernameBox";
-            this.UsernameBox.Size = new System.Drawing.Size(230, 26);
+            this.UsernameBox.Size = new System.Drawing.Size(155, 20);
             this.UsernameBox.TabIndex = 3;
-            this.UsernameBox.Text = "input username";
             // 
             // PasswordBox
             // 
-            this.PasswordBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PasswordBox.ForeColor = System.Drawing.SystemColors.InactiveCaption;
-            this.PasswordBox.Location = new System.Drawing.Point(476, 331);
-            this.PasswordBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PasswordBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordBox.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.PasswordBox.Location = new System.Drawing.Point(317, 215);
             this.PasswordBox.Name = "PasswordBox";
-            this.PasswordBox.Size = new System.Drawing.Size(230, 26);
+            this.PasswordBox.Size = new System.Drawing.Size(155, 20);
             this.PasswordBox.TabIndex = 4;
-            this.PasswordBox.Text = "input username";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.RosyBrown;
-            this.label2.Location = new System.Drawing.Point(309, 223);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(206, 145);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.Size = new System.Drawing.Size(75, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Username : ";
             // 
@@ -100,30 +93,29 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.RosyBrown;
-            this.label3.Location = new System.Drawing.Point(312, 331);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(208, 215);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(109, 20);
+            this.label3.Size = new System.Drawing.Size(73, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Password : ";
             // 
             // LoginBtn
             // 
-            this.LoginBtn.Location = new System.Drawing.Point(508, 535);
-            this.LoginBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.LoginBtn.Location = new System.Drawing.Point(339, 348);
             this.LoginBtn.Name = "LoginBtn";
-            this.LoginBtn.Size = new System.Drawing.Size(146, 52);
+            this.LoginBtn.Size = new System.Drawing.Size(97, 34);
             this.LoginBtn.TabIndex = 7;
             this.LoginBtn.Text = "Login";
             this.LoginBtn.UseVisualStyleBackColor = true;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.BackgroundImage = global::go2grocery_1.Properties.Resources.go2grocery;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.LoginBtn);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -131,7 +123,6 @@
             this.Controls.Add(this.UsernameBox);
             this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "LoginForm";
             this.Text = "LoginForm";
             this.Load += new System.EventHandler(this.LoginForm_Load);
@@ -141,6 +132,8 @@
         }
 
         #endregion
+        private DataSet1TableAdapters.LoginTableAdapter LoginTableAdapter;
+        private DataSet1TableAdapters.TableAdapterManager tableAdapterManager;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox UsernameBox;
--- go2grocery_1/LoginForm.cs
+++ go2grocery_1/LoginForm.cs
@@ -24,7 +24,26 @@ namespace go2grocery_1
 
         private void LoginForm_Load(object sender, EventArgs e)
         {
+            this.LoginTableAdapter.GetData();
+        }
 
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            if (UsernameBox.Text == "" && PasswordBox.Text == "") 
+            {
+                MessageBox.Show("Please enter a username and password");
+            }
+            else if (UsernameBox.Text == "")
+            {
+                MessageBox.Show("Please enter a username");
+            }
+            else if (PasswordBox.Text == "")
+            {
+                MessageBox.Show("Please enter a password");
+            }
+            //Form1 form1 = new Form1();
+            //form1.Show();
+            //this.Hide();
         }
     }
 }
